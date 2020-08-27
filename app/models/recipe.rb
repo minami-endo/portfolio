7ab80ed70705397b6f5b64cbf96ca8855cd9ea07
vagrant <vagrant@localhost.localdomain>
2020-08-27 03:30:04 +0000

    [Fix]previous_period_top

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -52,27 +52,27 @@ class Recipe < ApplicationRecord
    end
 
    def self.previous_monthly_top
-   beginning_of_month = previous_period[:start_month]
-   end_of_month = previous_period[:end_month]
+   pre_beginning_of_month = previous_period[:previous_start_month]
+   pre_end_of_month = previous_period[:previous_end_month]
    joins(:likes).merge(Like.group(:recipe_id).order('count(recipe_id) desc'))
-                .where(created_at: beginning_of_month..end_of_month).first
+                .where(created_at: pre_beginning_of_month..pre_end_of_month).first
    end
 
    def self.previous_period
     year = Time.now.strftime('%Y').to_i
-    previous_start_month = [1, 4, 7, 10]
-    previous_end_month = [3, 6, 9, 12]
-    beginning_of_month_list = previous_start_month.map { |m| Time.new(year, m).beginning_of_month }
-    end_of_month_list = previous_end_month.map { |m| Time.new(year, m).end_of_month }
-    previous_periods = beginning_of_month_list.zip(end_of_month_list)
+    previous_start_months = [1, 4, 7, 10]
+    previous_end_months = [3, 6, 9, 12]
+    pre_beginning_of_month_list = previous_start_months.map { |m| Time.new(year, m).beginning_of_month }
+    pre_end_of_month_list = previous_end_months.map { |m| Time.new(year, m).end_of_month }
+    previous_periods = pre_beginning_of_month_list.zip(pre_end_of_month_list)
     previous_start_month = 0
-    previous_start_month = 0
+    previous_end_month = 0
     previous_periods.each do |period|
-      if period[0] < DateTime.now && DateTime.now < period[1]
-        previous_start_month = period[(0-1).to_i]
-        previous_end_month = period[(1-1).to_i]
+      if period[0] < DateTime.now.prev_month(3) && DateTime.now.prev_month(3) < period[1]
+        previous_start_month = period[0]
+        previous_end_month = period[1]
       end
     end
-    { start_month: previous_start_month, end_month: previous_end_month }
+    { previous_start_month: previous_start_month, previous_end_month: previous_end_month }
    end
 end
