33f07db0e7bb8997e8c8bc606c78feda3bc9f5ed
vagrant <vagrant@localhost.localdomain>
2020-10-09 08:59:17 +0000

    [Finish]動的フォームの追加・削除

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -36,13 +36,11 @@ class Public::RecipesController < ApplicationController
 
   def edit
     @recipe = Recipe.find(params[:id])
-    @ingredients = @recipe.ingredients.where(deleted_flag: false)
   end
 
   def update
     @recipe = Recipe.find(params[:id])
     @recipe.update(recipe_params)
-    byebug
     redirect_to public_recipes_path
   end
 
@@ -55,7 +53,7 @@ class Public::RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:user_id, :item_id, :image, :name, :time, :difficulty_level, :how_to_make, ingredients_attributes: [:id, :recipe_id, :name])
+    params.require(:recipe).permit(:user_id, :item_id, :image, :name, :time, :difficulty_level, :how_to_make, ingredients_attributes: [:id, :recipe_id, :name, :deleted_flag])
   end
 
   def search_params
